67e6e75179cd3b8346f25cca5511035edfa27707
Bartosz Bąbol <bartosz.babol@javeo.eu>
2015-12-03 17:06:18 +0100

    Add cors support

--- api/src/main/scala/Main.scala
+++ api/src/main/scala/Main.scala
@@ -1,13 +1,15 @@
 
+import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.model.headers.{`Access-Control-Allow-Credentials`, `Access-Control-Max-Age`, `Access-Control-Allow-Headers`}
 import akka.stream.ActorMaterializer
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
-import utils.Config
+import utils.{CorsSupport, Config}
 import scala.concurrent.ExecutionContext
 import akka.http.scaladsl.server.Directives._
 
-object Main extends App with Config with Routes {
+object Main extends App with Config with Routes with CorsSupport{
   private implicit val system = ActorSystem()
   protected implicit val executor: ExecutionContext = system.dispatcher
   protected val log: LoggingAdapter = Logging(system, getClass)
--- api/src/main/scala/Routes.scala
+++ api/src/main/scala/Routes.scala
@@ -1,17 +1,30 @@
+import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.model.headers.{`Access-Control-Allow-Credentials`, `Access-Control-Max-Age`, `Access-Control-Allow-Headers`}
 import akka.http.scaladsl.server.Directives._
 import api._
+import utils.CorsSupport
 
 trait Routes extends LecturesListApi
             with LecturesDetailsApi
             with LecturesCreateApi
             with LecturesDeleteApi
-            with LecturesUpdateApi {
+            with LecturesUpdateApi
+            with CorsSupport{
+
+  override val corsAllowOrigins: List[String] = List("*")
+  override val corsAllowedHeaders: List[String] = List("Origin", "X-Requested-With", "Content-Type", "Accept", "Accept-Encoding", "Accept-Language", "Host", "Referer", "User-Agent")
+  override val corsAllowCredentials: Boolean = true
+  override val optionsCorsHeaders: List[HttpHeader] = List[HttpHeader](
+    `Access-Control-Allow-Headers`(corsAllowedHeaders.mkString(", ")),
+    `Access-Control-Max-Age`(60 * 60 * 24 * 20), // cache pre-flight response for 20 days
+    `Access-Control-Allow-Credentials`(corsAllowCredentials)
+  )
 
   val routes = pathPrefix("v1") {
-    lecturesListRoute ~
-    lectureDetailsRoute ~
-    lectureCreateRoute ~
-    lectureDeleteRoute ~
-    lectureUpdateRoute
+    cors{lecturesListRoute} ~
+    cors{lectureDetailsRoute}~
+    cors{lectureCreateRoute}~
+    cors{lectureDeleteRoute}~
+    cors{lectureUpdateRoute}
   } ~ path("")(getFromResource("public/index.html"))
 }
